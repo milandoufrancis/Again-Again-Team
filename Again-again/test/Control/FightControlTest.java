dc3714cb2f5f7842760ace48324b309d23431b44
milandoufrancis <mil15083@byui.edu>
2018-06-04 13:26:47 -0600

    6/4/2018, I was working on trap control method, and It passed all the tests.

--- Again-again/test/Control/FightControlTest.java
+++ Again-again/test/Control/FightControlTest.java
@@ -13,7 +13,7 @@ import static org.junit.Assert.*;
  * @author My Laptop
  */
 public class FightControlTest {
-    
+
     public FightControlTest() {
     }
 
@@ -29,7 +29,7 @@ public class FightControlTest {
         double expResult = 76.0;
         double result = FightControl.fightControl(Health, Defense, Weapon);
         assertEquals(expResult, result, 0.0);
-        
+
         System.out.println("Test 2");
         Health = 60.0;
         Defense = 6.0;
@@ -37,7 +37,7 @@ public class FightControlTest {
         expResult = 56.0;
         result = FightControl.fightControl(Health, Defense, Weapon);
         assertEquals(expResult, result, 0.0);
-       
+
         System.out.println("Test 3");
         Health = -1.0;
         Defense = 32.3;
@@ -45,8 +45,62 @@ public class FightControlTest {
         expResult = -1;
         result = FightControl.fightControl(Health, Defense, Weapon);
         assertEquals(expResult, result, 0.0);
-       
-       
+
     }
-    
+
+    /**
+     * Test of trapcontrol method, of class FightControl.
+     */
+    @Test
+    public void testTrapcontrol() {
+        System.out.println("test 1");
+        double Health = 80.0;
+        double Trap = 6.0;
+        double Level = 10.0;
+        double expResult = 60.0;
+        double result = FightControl.trapcontrol(Health, Trap, Level);
+        assertEquals(expResult, result, 0.0);
+        
+        System.out.println("test 2");
+         Health = 102.0;
+         Trap = 8.0;
+         Level = 4.0;
+         expResult = -1;
+         result = FightControl.trapcontrol(Health, Trap, Level);
+        assertEquals(expResult, result, 0.0);
+        
+        
+        System.out.println("test 3");
+         Health = 92.0;
+         Trap = 0.0;
+         Level = 3.0;
+         expResult = -1;
+         result = FightControl.trapcontrol(Health, Trap, Level);
+        assertEquals(expResult, result, 0.0);
+        
+        System.out.println("test 4");
+         Health = 96.0;
+         Trap = 4.0;
+         Level = -1.0;
+         expResult = -1;
+         result = FightControl.trapcontrol(Health, Trap, Level);
+        assertEquals(expResult, result, 0.0);
+        
+        System.out.println("test 5");
+         Health = 1.0;
+         Trap = 1.0;
+         Level = 1.0;
+         expResult = 0.66666667;
+         result = FightControl.trapcontrol(Health, Trap, Level);
+        assertEquals(expResult, result, 0.1);
+        
+        System.out.println("test 6");
+         Health = 100.0;
+         Trap = 10.0;
+         Level = 10.0;
+         expResult = 66.6666667;
+         result = FightControl.trapcontrol(Health, Trap, Level);
+        assertEquals(expResult, result, 0.1);
+    }
+
 }
