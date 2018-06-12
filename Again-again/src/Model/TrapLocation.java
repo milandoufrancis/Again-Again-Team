3a2bc78deafd9e49832bf24bef6fe9f20174dc1a
levsarg3 <sar16004@byui@edu>
2018-06-12 13:08:30 -0600

    Continued work on Team Assignment 7. Created several new control methods: GameControl, and MainMenuView. June 11, 2018
    -Levi Sargeant

---
+++ Again-again/src/Model/TrapLocation.java
@@ -0,0 +1,70 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+import java.util.Objects;
+
+/**
+ *
+ * @author Levi
+ */
+public class TrapLocation extends Location{
+    private String trapType;
+    private int trapScore;
+
+    public String getTrapType() {
+        return trapType;
+    }
+
+    public void setTrapType(String trapType) {
+        this.trapType = trapType;
+    }
+
+    public int getTrapScore() {
+        return trapScore;
+    }
+
+    public void setTrapScore(int trapScore) {
+        this.trapScore = trapScore;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 97 * hash + Objects.hashCode(this.trapType);
+        hash = 97 * hash + this.trapScore;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final TrapLocation other = (TrapLocation) obj;
+        if (this.trapScore != other.trapScore) {
+            return false;
+        }
+        if (!Objects.equals(this.trapType, other.trapType)) {
+            return false;
+        }
+        return true;
+    }
+
+    
+    @Override
+    public String toString() {
+        return "TrapLocation{" + "trapType=" + trapType + ", trapScore=" + trapScore + '}';
+    }
+    
+    
+}
