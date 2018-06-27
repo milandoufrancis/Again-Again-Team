38665022b6b007e0c371b63af5c7340aa95770fa
levsarg3 <sar16004@byui@edu>
2018-06-27 13:02:27 -0600

    Merge origin/master
    Conflicts:
    	Again-again/src/byui/cit260/againagain/view/TrapMenuView.java

--- Again-again/test/Control/FightControlTest.java
+++ Again-again/test/Control/FightControlTest.java
@@ -23,25 +23,25 @@ public class FightControlTest {
     @Test
     public void testFightControl() {
         System.out.println("Test 1");
-        double Health = 80.0;
-        double Defense = 5.0;
-        double Weapon = 20.0;
-        double expResult = 76.0;
-        double result = FightControl.fightControl(Health, Defense, Weapon);
+        int Health = 80;
+        int Defense = 5;
+        int Weapon = 20;
+        int expResult = 76;
+        int result = FightControl.fightControl(Health, Defense, Weapon);
         assertEquals(expResult, result, 0.0);
 
         System.out.println("Test 2");
-        Health = 60.0;
-        Defense = 6.0;
-        Weapon = 24.0;
-        expResult = 56.0;
+        Health = 60;
+        Defense = 6;
+        Weapon = 24;
+        expResult = 56;
         result = FightControl.fightControl(Health, Defense, Weapon);
         assertEquals(expResult, result, 0.0);
 
         System.out.println("Test 3");
-        Health = -1.0;
-        Defense = 32.3;
-        Weapon = 25.5;
+        Health = -1;
+        Defense = 32;
+        Weapon = 25;
         expResult = -1;
         result = FightControl.fightControl(Health, Defense, Weapon);
         assertEquals(expResult, result, 0.0);
@@ -54,53 +54,53 @@ public class FightControlTest {
     @Test
     public void testTrapcontrol() {
         System.out.println("test 1");
-        double Health = 80.0;
-        double Trap = 6.0;
-        double Level = 10.0;
-        double expResult = 60.0;
-        double result = FightControl.trapControl(Health, Trap, Level);
+        int Health = 80;
+        int Trap = 6;
+        int Level = 10;
+        int expResult = 62;
+        int result = FightControl.trapControl(Health, Trap, Level);
         assertEquals(expResult, result, 0.0);
         
         System.out.println("test 2");
-         Health = 102.0;
-         Trap = 8.0;
-         Level = 4.0;
+         Health = 102;
+         Trap = 8;
+         Level = 4;
          expResult = -1;
          result = FightControl.trapControl(Health, Trap, Level);
         assertEquals(expResult, result, 0.0);
         
         
         System.out.println("test 3");
-         Health = 92.0;
-         Trap = 0.0;
-         Level = 3.0;
+         Health = 92;
+         Trap = 0;
+         Level = 3;
          expResult = -1;
          result = FightControl.trapControl(Health, Trap, Level);
         assertEquals(expResult, result, 0.0);
         
         System.out.println("test 4");
-         Health = 96.0;
-         Trap = 4.0;
-         Level = -1.0;
+         Health = 96;
+         Trap = 4;
+         Level = -1;
          expResult = -1;
          result = FightControl.trapControl(Health, Trap, Level);
         assertEquals(expResult, result, 0.0);
         
         System.out.println("test 5");
-         Health = 1.0;
-         Trap = 1.0;
-         Level = 1.0;
-         expResult = 0.66666667;
+         Health = 1;
+         Trap = 1;
+         Level = 1;
+         expResult = 0.0;
          result = FightControl.trapControl(Health, Trap, Level);
-        assertEquals(expResult, result, 0.1);
+        assertEquals(expResult, result, 0.0);
         
         System.out.println("test 6");
-         Health = 100.0;
-         Trap = 10.0;
-         Level = 10.0;
-         expResult = 66.6666667;
+         Health = 100;
+         Trap = 10;
+         Level = 10;
+         expResult = 66;
          result = FightControl.trapControl(Health, Trap, Level);
-        assertEquals(expResult, result, 0.1);
+        assertEquals(expResult, result, 0.0);
     }
 
 }
