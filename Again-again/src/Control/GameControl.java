3bd34a15e2a9224b7cbb956f449aeaa05a4258af
levsarg3 <sar16004@byui@edu>
2018-07-02 13:46:51 -0600

    July 2, 2018 -Set skeleton actor in the map. We just need to make more actors as well as set the main player -Changed up the way we are going to assign actors in case we want to add more actors (i.e. more new monsters)
    Levi Sargeant

--- Again-again/src/Control/GameControl.java
+++ Again-again/src/Control/GameControl.java
@@ -6,6 +6,7 @@
 package Control;
 
 import Model.Actor;
+import Model.ActorType;
 import Model.Game;
 import Model.Item;
 import Model.Location;
@@ -33,7 +34,7 @@ public class GameControl {
     }
 
     public static Actor[] createActors() {
-        Actor[] actors = new Actor[6];
+        Actor[] actors = new Actor[ActorType.values().length];
         //Actor for the player's character
         Actor mainplayer = new Actor();
         mainplayer.setDescription("This is you, the main character of this game!");
@@ -42,7 +43,7 @@ public class GameControl {
         mainplayer.setActorDamage(3);
         mainplayer.setDefence(5);
         mainplayer.setActorScore(1);
-        actors[0] = mainplayer;
+        actors[ActorType.mainplayer.ordinal()] = mainplayer;
 
         //Actor object for the monster, Werewolf
         Actor werewolf = new Actor();
