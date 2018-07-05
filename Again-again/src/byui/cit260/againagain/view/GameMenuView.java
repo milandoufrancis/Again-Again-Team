3d7d0df8d55947a3a1e1bceaca69f8ab9daeffbd
milandoufrancis <mil15083@byui.edu>
2018-07-05 15:22:54 -0600

    7/3/2018, I have implemented the display map option in the Game Menu.

--- Again-again/src/byui/cit260/againagain/view/GameMenuView.java
+++ Again-again/src/byui/cit260/againagain/view/GameMenuView.java
@@ -5,6 +5,10 @@
  */
 package byui.cit260.againagain.view;
 
+import Model.Game;
+import Model.Location;
+import again.again.AgainAgain;
+
 /**
  *
  * @author My Laptop
@@ -29,7 +33,7 @@ class GameMenuView extends View{
         //System.out.println("doaction called");
         //String menuItem = inputs[0];
 
-        switch (selection) {
+        switch (inputs[0]) {
             case "V":// to view location
                 this.displayMap();
                 break;
@@ -48,6 +52,51 @@ class GameMenuView extends View{
         }
         return true;
         
+    }
+
+    private void displayMap() {
+        //System.out.println("***displaymap stub function called***");
+        //game = get the currentGame from the main class
+        Game game = AgainAgain.getGame();
+//locations = get the 2-D locations array from the map
+Location[][] locations = game.getMap().getLocations();
+//Print the title
+System.out.println("Again&Again dark dungeaon");
+//Print the column numbers for each column
+System.out.println("1|2|3|4|5");
+//for every row in map
+for (int row = 0; row < locations.length; row++){
+//Print a row divider
+    System.out.println("1-2-3-4-5");
+//Print the row number on a new line
+System.out.print(row + 1);
+//for every column in the row
+for (int col = 0; col < locations[row].length; col++);{
+ //Print a column divider
+System.out.println("|");
+}
+
+location = locations[row][column]
+if location has been visited
+Get the mapSymbol for the scene in this location
+Print the mapSymbol
+else
+Print " ?? "
+endif
+endFor
+Print the ending column divider
+        }
+endFor
+Print the ending row divider
+        
+    }
+
+    private void viewItem() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private void viewActors() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     
