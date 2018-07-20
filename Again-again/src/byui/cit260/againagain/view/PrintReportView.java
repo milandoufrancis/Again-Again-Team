bc182d83364e2eb47e4410e04fa738de7a71b99a
milandoufrancis <mil15083@byui.edu>
2018-07-20 13:17:32 -0600

    7/20/2018 I just created the print report view.

---
+++ Again-again/src/byui/cit260/againagain/view/PrintReportView.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.againagain.view;
+
+/**
+ *
+ * @author My Laptop
+ */
+public class PrintReportView extends View{
+
+    @Override
+    public String[] getInputs() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean doAction(String[] inputs) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
